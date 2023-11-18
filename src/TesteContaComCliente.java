91da098c99b94fbceada31428133f788b23ca087
junyorff12 <junyorff12@gmail.com>
2023-11-17 21:41:59 -0300

    Encapsulamento.

--- src/TesteContaComCliente.java
+++ src/TesteContaComCliente.java
@@ -3,15 +3,15 @@ public class TesteContaComCliente {
 
 	public static void main(String[] args) {
 		Cliente cliente = new Cliente();
-		cliente.nome = "Junyorff12";
-		cliente.cpf = "222-222-222-22";
-		cliente.profissao = "desenvolvedor Backend Java";
+		cliente.setNome("Junyorff11");
+		cliente.setCpf("222-222-222-22");
+		cliente.setProfissao("desenvolvedor Backend Java");
 		
 		Conta conta = new Conta();
-		conta.titular = cliente;
+		conta.setTitular(cliente);
 		
-		System.out.println(conta.titular.nome);
-		System.out.println(conta.titular);
+		System.out.println(conta.getTitular().getNome());
+		System.out.println(conta.getTitular());
 
 	}
 
